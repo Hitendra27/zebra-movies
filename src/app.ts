bd3dd389e0e4f97b704e7b3ad6a0f63a6f6d3643
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-08-02 15:15:27 +0400

    feat: Implement fetchAPI to get movie data

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
-import express from "express";
+import express, { response } from "express";
 import { router } from "./routes/routes";
 
 export const app = express();
 
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Welcome to the Movie API!"));
+
+app.get('/test', (req, res) => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then ((data) => {
+        console.log(data)
+    })
+
+})
+
+
 
 app.use("/", router);
