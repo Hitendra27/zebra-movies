21f05fcf9f18129f633b889ffcd7ab59da65b737
Atty Cronin <antoniajcronin@gmail.com>
2023-08-03 15:16:02 +0100

    added comment

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 export const app = express();
 
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
-
+// comment!!!
 app.options('*', cors({
     origin: true,
     credentials: true
