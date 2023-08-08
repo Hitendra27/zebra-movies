7bada231136ac96beefa7a4c21f2b811aaa442b8
Zichao Zang <zang.code@gmail.com>
2023-08-09 00:11:34 +0100

    refactor search and filter endpoints

--- src/controllers/movie_controller.ts
+++ src/controllers/movie_controller.ts
@@ -3,74 +3,79 @@ import * as movieService from "../services/movie_service";
 import { MovieResponse } from "../services/movie_service";
 
 export const getHealth = async (
-  req: Request<object, object, object>,
-  res: Response
+    req: Request<object, object, object>,
+    res: Response
 ) => {
-  console.log("Health check:  Server is running!");
-  res.status(200).send("Server is running");
-};
+    console.log('Health check:  Server is running!')
+    res.status(200).send('Server is running')
+}
 
 export const getMovies = async (
-  req: Request<object, object, object>,
-  res: Response
+    req: Request<object, object, object>,
+    res: Response
 ) => {
-  const movies = await movieService.getMovies();
-  res.json(movies).status(200);
+    const movies = await movieService.getMovies();
+    res.json(movies).status(200);
 };
 
 export const getSearch = async (
-  req: Request<object, object, object, { query: string; genres: string[] }>,
-  res: Response
+    req: Request<object, object, object, { query: string, genres: string }>,
+    res: Response
 ) => {
-  const { query, genres } = req.query;
-  const filterByGenreResults = await movieService.filterByGenres({ genres });
-  const searchByStringResults = await movieService.searchByString({ query });
+    const { query, genres } = req.query;
 
-  if (filterByGenreResults === null) return res.sendStatus(400);
-  if (searchByStringResults === null) return res.sendStatus(400);
+    const filterByGenreResults = await movieService.filterByGenres({ genres });
+    const searchByStringResults = await movieService.searchByString({ query });
+    const allResults = await movieService.getMovies();
 
-  const combinedResults = searchByStringResults.reduce(
-    (acc: MovieResponse[], item: MovieResponse) => {
-      return acc.includes(item) ? acc : [...acc, item];
-    },
-    [...filterByGenreResults]
-  );
+    if (filterByGenreResults === null) return res.sendStatus(400);
+    if (searchByStringResults === null) return res.sendStatus(400);
 
-  res.status(200);
-  res.send(combinedResults);
+    res.status(200);
+
+    if (genres === '' && query === '') {
+        res.send(allResults);
+    } else if (genres !== '' && query === '') {
+        console.log('genres', genres)
+        res.send(filterByGenreResults);
+    } else if (genres !== '' && query !== '') {
+        const genreArray = genres?.split(',')
+        const combinedResults = searchByStringResults.filter(movie => movie.genre_ids.some(id => genreArray.includes(id.toString())))
+        res.send(combinedResults)
+    }
 };
 
 export const getGenres = async (
-  req: Request<object, object, object>,
-  res: Response
+    req: Request<object, object, object>,
+    res: Response
 ) => {
-  const genreResults = await movieService.getGenres();
-  if (!genreResults) return res.sendStatus(400);
+    const genreResults = await movieService.getGenres();
+    if (!genreResults) return res.sendStatus(400);
 
-  res.status(200);
-  res.send(genreResults);
+    res.status(200);
+    res.send(genreResults);
 };
 
 // latest movies
 export const getLatest = async (
-  req: Request<object, object, object>,
-  res: Response
+    req: Request<object, object, object>,
+    res: Response
 ) => {
-  const latestResults = await movieService.getLatest();
-  if (!latestResults) return res.sendStatus(400);
+    const latestResults = await movieService.getLatest();
+    if (!latestResults) return res.sendStatus(400);
 
-  res.status(200);
-  res.send(latestResults);
+    res.status(200);
+    res.send(latestResults);
 };
 
 // Upcoming movies
 export const getUpcomingMovies = async (
-  req: Request<object, object, object>,
-  res: Response
+    req: Request<object, object, object>,
+    res: Response
 ) => {
-  const upcomingMoviesResults = await movieService.getUpcomingMovies();
-  if (!upcomingMoviesResults) return res.sendStatus(400);
+    const upcomingMoviesResults = await movieService.getUpcomingMovies();
+    if (!upcomingMoviesResults) return res.sendStatus(400);
 
-  res.status(200);
-  res.send(upcomingMoviesResults);
+    res.status(200);
+    res.send(upcomingMoviesResults);
 };
--- src/services/movie_service.ts
+++ src/services/movie_service.ts
@@ -1,117 +1,127 @@
 export interface MovieResponse {
-  id: number;
-  title: string;
-  overview: string;
-  poster_path: string;
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+}
+export interface MovieWithGenreResponse {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    genre_ids: number[]
 }
 
 const API_URL =
-  `${process.env.BASE_URL}discover/movie?sort_by=popularity.desc&api_key=${process.env.API_KEY}&page=1`;
+    `${process.env.BASE_URL}discover/movie?sort_by=popularity.desc&api_key=${process.env.API_KEY}&page=1`;
 
 export const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzJhNDJlYjU3YTVjM2Y5Y2U4Y2U1YmY4M2U1NjIwMSIsInN1YiI6IjY0Y2JiZWU1Nzg1NzBlMDExZTUzZjZhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bSkIGEeiWScqFM0nIyIPqBzyM1A1EgtGpz0xJ0akZgc",
-  },
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzJhNDJlYjU3YTVjM2Y5Y2U4Y2U1YmY4M2U1NjIwMSIsInN1YiI6IjY0Y2JiZWU1Nzg1NzBlMDExZTUzZjZhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bSkIGEeiWScqFM0nIyIPqBzyM1A1EgtGpz0xJ0akZgc",
+    },
 };
 
 export const getMovies = async (): Promise<MovieResponse[]> => {
-  try {
-    const response = await fetch(API_URL);
-    if (!response.ok) {
-      throw new Error("Bad response");
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw new Error("Bad response");
+        }
+        const data = await response.json();
+        const filteredMovies: MovieResponse[] = data.results.map((movie: any) => ({
+            id: movie.id,
+            title: movie.title,
+            overview: movie.overview,
+            poster_path: movie.poster_path
+        }));
+        return filteredMovies;
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+        throw new Error("An error occurred while fetching movies");
     }
-    const data = await response.json();
-    const filteredMovies: MovieResponse[] = data.results.map((movie: any) => ({
-      id: movie.id,
-      title: movie.title,
-      overview: movie.overview,
-      poster_path: movie.poster_path,
-    }));
-    return filteredMovies;
-  } catch (error) {
-    console.error("Error fetching movies:", error);
-    throw new Error("An error occurred while fetching movies");
-  }
 };
 
-export const searchByString = async ({ query }: { query: string }) => {
-  try {
-    const url = `${process.env.BASE_URL}search/movie?query=${query}&include_adult=false&language=en-US&api_key=${process.env.API_KEY}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const searchMovieResults: MovieResponse[] = data.results.map(
-      (movie: any) => ({
-        id: movie.id,
-        title: movie.title,
-        overview: movie.overview,
-        poster_path: movie.poster_path,
-      })
-    );
-    return searchMovieResults;
-  } catch (error) {
-    console.error("Error fetching search by string:", error);
-    return null;
-  }
+export const searchByString = async ({
+    query = '',
+}: {
+    query: string;
+}) => {
+    try {
+        const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&api_key=${process.env.API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const searchMovieResults: MovieWithGenreResponse[] = data.results.map((movie: any) => ({
+            id: movie.id,
+            title: movie.title,
+            overview: movie.overview,
+            poster_path: movie.poster_path,
+            genre_ids: movie.genre_ids
+        }));
+        return searchMovieResults;
+    } catch (error) {
+        console.error("Error fetching search by string:", error);
+        return null;
+    }
 };
 
-export const filterByGenres = async ({ genres }: { genres: string[] }) => {
-  const genreString = genres.join("|"); // search performed with OR operator
-  const url = `${process.env.BASE_URL}discover/movie?include_adult=false&include_video=false&language=en-GB&with_genres=${genreString}`;
+export const filterByGenres = async ({
+    genres = ''
+}: { genres: string }) => {
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-GB&with_genres=${genres}&api_key=${process.env.API_KEY}`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    const genreFilterResults: MovieResponse[] = data.results.map(
-      (movie: any) => ({
-        id: movie.id,
-        title: movie.title,
-        overview: movie.overview,
-        poster_path: movie.poster_path,
-      })
-    );
-    return genreFilterResults;
-  } catch (error) {
-    console.error("Error fetching filter by genre:", error);
-    return null;
-  }
-};
+    try {
+        const response = await fetch(url)
+        const data = await response.json();
+        const genreFilterResults: MovieResponse[] = data.results.map((movie: any) => ({
+            id: movie.id,
+            title: movie.title,
+            overview: movie.overview,
+            poster_path: movie.poster_path
+        }));
+        return genreFilterResults
+    } catch (error) {
+        console.error("Error fetching filter by genre:", error);
+        return null;
+    }
+}
 
 export const getGenres = async () => {
-  try {
-    const url = `${process.env.BASE_URL}genre/movie/list?language=en`;
-    const response = await fetch(url, options);
-    const genreResults = await response.json();
-    return genreResults;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+    try {
+        const url = `https://api.themoviedb.org/3/genre/movie/list?language=en`;
+        const response = await fetch(url, options);
+        const genreResults = await response.json();
+        return genreResults;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
 export const getLatest = async () => {
-  try {
-    const url = `${process.env.BASE_URL}discover/movie?api_key=${process.env.API_KEY}&page=1&include_video=false&primary_release_date.gte=2023-08-07&primary_release_date.lte=2022-03-01`;
-    const response = await fetch(url, options);
-    const latestResults = await response.json();
-    return latestResults;
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
+    try {
+        const url = `https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&primary_release_date.gte=2023-08-07&primary_release_date.lte=2022-03-01?api_key=${process.env.API_KEY}`;
+        const response = await fetch(url, options);
+        const latestResults = await response.json();
+        return latestResults;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 };
 
 export const getUpcomingMovies = async () => {
-  try {
-    const url = `${process.env.BASE_URL}movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1&region=US`;
-    const response = await fetch(url, options);
-    const upcomingMoviesResults = await response.json();
-    return upcomingMoviesResults;
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
+    try {
+        const url = `${process.env.BASE_URL}movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1&region=US`;
+        const response = await fetch(url, options);
+        const upcomingMoviesResults = await response.json();
+        return upcomingMoviesResults;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 };
 
